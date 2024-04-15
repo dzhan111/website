12836ba76ca4878a033164408267459a4af98ab8
dzhan111 <zhandavid4@gmail.com>
2024-04-15 15:00:07 -0400

    new

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import HelloWorld from './Components/HelloWorld';
 function App() {
   return (
     <div>
-      <HelloWorld name="besdfnas"/>
-      <p>TMy name is David and I a;knm a CIS student</p>
+      <HelloWorld name="page"/>
+      
+      <p>My name is David and I am a CIS student</p>
 
     </div>
   );
