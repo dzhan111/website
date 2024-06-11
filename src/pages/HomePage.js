8af866f0667da42bf1a081eae0e1a244774ebe08
dzhan111 <zhandavid4@gmail.com>
2024-06-11 10:33:59 -0400

    desciption

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,9 @@ const HomePage = () => {
     return <>
         <p className='intro'>Hi, My name is David.</p>
         <p className='introduction'>I am currently a rising junior at the University of Pennsylvania studying Computer Science.
-             My interests include data analytics, full stack development, and Machine Learning. </p>
+             My interests include data analytics, full stack development, and Machine Learning.  Currently for Summer 2024, I am 
+            working as a Software Engineering Intern for a UMD based biotech startup called Ventoscity.
+             </p>
     
     
     </>;
